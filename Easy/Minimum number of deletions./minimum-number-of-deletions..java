f34f6b996ac937a64000aef4a9d943dfd16ad184
yash-tingre-10 <86177587+yash-tingre-10@users.noreply.github.com>
2023-01-08 18:17:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum number of deletions./minimum-number-of-deletions..java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            int n = Integer.parseInt(read.readLine());
+            String str = read.readLine().strip();
+            Solution ob = new Solution();
+            long ans = ob.minDeletions(str, n); 
+            System.out.println(ans);
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    static int dp [][];
+    int minDeletions(String s1, int n)
+    {
+        
+        String s2 = new StringBuilder(s1).reverse().toString();
+        int m = s2.length();
+        dp = new int [n+1][m+1];
+        int ans = helper(s1,s2,n,m);
+        return n - ans;
+    }
+    
+    static int helper(String s1, String s2, int n, int m){
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
+} 
